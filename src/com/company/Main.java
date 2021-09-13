bba197daa8f86a51b7f363bd510cf00beefa5734
1ntr0men <vadruf07@gmail.com>
2021-09-14 00:16:25 +0300

    sixth task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,27 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Arrays;
+
+
 public class Main {
+    public static void getMass(int[] mass) {
+        for (int i : mass) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello world");
+        Random objGenerator = new Random();
+        int mass[] = new int[10];
+        for (int i = 0; i < mass.length; i++) {
+//            int randomNumber = objGenerator.nextInt(100);
+            int randomNumber = (int) (Math.random() * 6);
+            mass[i] = randomNumber;
+        }
+        getMass(mass);
+        Arrays.sort(mass);
+        getMass(mass);
     }
 }
