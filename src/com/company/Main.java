2ea22fc98cbc2b336c090c8da290145742e04730
1ntr0men <vadruf07@gmail.com>
2021-09-14 09:49:03 +0300

    sixth task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static int factorial(int n) {
+        int s = 1;
+        for (int i = 1; i <= n; i++) {
+            s *= i;
+        }
+        return s;
+    }
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world");
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        System.out.println(factorial(num));
     }
 }
